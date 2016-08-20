d1e19b244fa5380d7654bba1e964575a790ec915
Augusto Bennemann <gutobenn@gmail.com>
2016-08-20 17:02:17 -0300

    Quantizacao arrumada

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# TODO exibir botões só quando a imagem já tiver sido carregada?
-# TODO limitar imagem ao tamanho da tela
-
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib, GdkPixbuf
+from gi.repository.GdkPixbuf import Pixbuf, InterpType
 from PIL import Image
 
+"""
+FotochoPI, a free (as in draft beer) image editor
+"""
+__author__ = "Augusto Bennemann"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "abennemann@inf.ufrgs.br"
+
+# TODO add a loading image while processing the image?
+
 class FPIWindow(Gtk.Window):
-    img = "" # TODO how to initialize it correctly?
-    pix = ""
-    gtkimg = Gtk.Image
+    temp_height = 0
+    temp_width = 0
 
     def __init__(self):
         Gtk.Window.__init__(self, title="FotochoPI")
@@ -23,7 +30,7 @@ class FPIWindow(Gtk.Window):
 
         hbox = Gtk.Box(spacing=6)
         imgbox = Gtk.Box()
-        imgbox.set_border_width(10) # TODO ajeitar essa borda
+        imgbox.set_border_width(0)
         grid.add(hbox)
         grid.attach_next_to(imgbox, hbox, Gtk.PositionType.BOTTOM, 1, 2)
 
@@ -52,6 +59,8 @@ class FPIWindow(Gtk.Window):
         hbox.pack_start(button, True, True, 0)
 
         self.gtkimage = Gtk.Image()
+        #TODO resize image on window resize
+        #self.gtkimage.connect('draw', self.on_image_resize, window)
         imgbox.add(self.gtkimage)
 
     def on_file_clicked(self, widget):
@@ -64,14 +73,10 @@ class FPIWindow(Gtk.Window):
 
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
-            print "Open clicked"
             print "File selected: " + dialog.get_filename()
             self.img = Image.open(dialog.get_filename())
             self.pix = self.img.load()
             self.update_image()
-
-        elif response == Gtk.ResponseType.CANCEL:
-            print "Cancel clicked"
 
         dialog.destroy()
 
@@ -109,42 +114,35 @@ class FPIWindow(Gtk.Window):
 
         for i in range(width):
             for j in range(height):
-                r, g, b = original_pix[i, j]
-                cinza = int(0.299*r + 0.587*g + 0.114*b)
+                red, green, blue = original_pix[i, j]
+                cinza = int(0.299*red + 0.587*green + 0.114*blue)
                 self.pix[i, j] = (cinza, cinza, cinza)
 
-    def on_luminance_clicked(self, button):
-        self.apply_luminance()
-        self.update_image()
-
-    def on_quantization_clicked(self, button):
+    def apply_quantization(self, tones):
         if not is_grayscale(self.img):
             self.apply_luminance()
 
         # TODO fazer otimizacao q manuel falou pra evitar que
         # os extremos sejam usados sem necessidade
-        # TODO o extremo branco nao ta sendo usado! ajeitar isso
-        # TODO colocar botao de range
         original_img = self.img.copy()
         original_pix = original_img.load()
         width, height = self.img.size
 
-        ntons = 4
         for i in range(width):
             for j in range(height):
-                color = int((original_pix[i, j][0]*ntons)/256 * 256/ntons)
-                # TODO remove comment after fixing
-                """print original_pix[i, j][0],
-                print " => ",
-                print color"""
+                color = int((original_pix[i, j][0]*tones)/256 * 256/(tones-1))
                 self.pix[i, j] = (color, color, color)
+        self.update_image()
+
+    def on_luminance_clicked(self, button):
+        self.apply_luminance()
         self.update_image()
 
     def on_save_clicked(self, widget):
         dialog = Gtk.FileChooserDialog("Please choose a file", self,
                                        Gtk.FileChooserAction.SAVE,
                                        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-                                       Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+                                        Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
 
         self.add_filters(dialog)
 
@@ -165,8 +163,55 @@ class FPIWindow(Gtk.Window):
 
         dialog.destroy()
 
+    def on_quantization_clicked(self, widget):
+        dialog = DialogQuantization(self)
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            self.apply_quantization(int(dialog.spinbutton.get_value()))
+
+        dialog.destroy()
+
     def update_image(self):
-        self.gtkimage.set_from_pixbuf(image2pixbuf(self.img))
+        # TODO Resize if image is bigger than window
+        pixbuf = image2pixbuf(self.img)
+        #self.gtkimage.set_from_pixbuf(pixbuf.scale_simple(200, 200, InterpType.BILINEAR))
+        self.gtkimage.set_from_pixbuf(pixbuf)
+
+    def on_image_resize(self, widget, event, window):
+        allocation = widget.get_allocation()
+        if self.temp_height != allocation.height or self.temp_width != allocation.width:
+            self.temp_height = allocation.height
+            self.temp_width = allocation.width
+            pixbuf = image2pixbuf(self.img).scale_simple(allocation.width,
+                                                         allocation.height,
+                                                         InterpType.BILINEAR)
+            widget.set_from_pixbuf(pixbuf)
+
+
+class DialogQuantization(Gtk.Dialog):
+
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "Quantization", parent, 0,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+
+        self.set_default_size(30, 70)
+
+        table = Gtk.Table(1, 2, True)
+        label = Gtk.Label("Tones: ")
+        # TODO set default value the number of tones the photo currently have
+        adjustment = Gtk.Adjustment(256, 2, 256, 1, 10, 0)
+        self.spinbutton = Gtk.SpinButton()
+        self.spinbutton.set_adjustment(adjustment)
+
+        table.attach(label, 0, 1, 0, 1)
+        table.attach(self.spinbutton, 1, 3, 0, 1)
+
+        box = self.get_content_area()
+        box.add(table)
+        self.show_all()
+
 
 def image2pixbuf(im): # From https://gist.github.com/mozbugbox/10cd35b2872628246140
     """Convert Pillow image to GdkPixbuf"""
